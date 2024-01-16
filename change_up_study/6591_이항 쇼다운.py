38dabeac0faf0e9a05d728eed5e9b020e4f71658
zuhye <mari5669@naver.com>
2024-01-16 22:21:43 +0900

    240116_이항 쇼다운

---
+++ change_up_study/6591_이항 쇼다운.py
@@ -0,0 +1,15 @@
+import sys
+from math import comb
+
+input = sys.stdin.readline
+
+while True:
+    n, k = map(int, input().split())
+
+    if n == 0 and k == 0:
+        break
+
+    result = comb(n, k)
+    print(result)
+
+

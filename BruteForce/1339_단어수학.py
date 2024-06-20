9fb81e8f5f8a28d2995cf2e94ed389ad3ae878b4
Zuhye <mari5669@naver.com>
2024-06-20 16:26:05 +0900

    1339_단어수학(브루트포스)

--- BruteForce/1339_단어수학.py
+++ BruteForce/1339_단어수학.py
@@ -4,4 +4,22 @@ input = sys.stdin.readline
 
 N = int(input())
 numbers = [input().strip() for _ in range(N)]
+find = dict()
+answer = 0
 
+for i in numbers:
+    length = len(i)
+    for j in i:
+        if j not in find:
+            find[j] = 10 ** (length - 1)
+        else:
+            find[j] += 10 ** (length - 1)
+        length -= 1
+result = sorted(find.values(), reverse=True)
+
+number = 9
+for i in result:
+    answer += i * number
+    number -= 1
+
+print(answer)

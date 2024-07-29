b231d29ee42234b4704c2acd8ab15fd8f4fb37f6
Zuhye <mari5669@naver.com>
2024-07-29 23:56:10 +0900

    16956_늑대의양(bfs)

---
+++ Graph Traversal/16956_늑대의양.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+R, C = map(int, input().split())
+board = [list(input().strip()) for _ in range(R)]
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+def bfs(x, y):
+
+    for d in range(4):
+        nx = x + dir[d][0]
+        ny = y + dir[d][1]
+
+        if 0 <= nx < R and 0 <= ny < C:
+            if board[nx][ny] == "S":
+                return False
+            if board[nx][ny] == ".":
+                board[nx][ny] = "D"
+    return True
+
+
+for i in range(R):
+    for j in range(C):
+        flag = True
+        if board[i][j] == "W":
+            flag = bfs(i, j)
+
+            if not flag:
+                print(0)
+                break
+
+if flag == True:
+    print(1)
+    for i in board:
+        print(''.join(i))

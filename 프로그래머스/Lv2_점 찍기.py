5cdd6ccca68713ce7a1a96912967aac96a35caca
Zuhye <mari5669@naver.com>
2024-10-01 18:27:07 +0900

    프로그래머스_점찍기(구현)

---
+++ 프로그래머스/Lv2_점 찍기.py
@@ -0,0 +1,19 @@
+import math
+
+
+def solution(k, d):
+    answer = 0
+    y = 0
+
+    for x in range(0, d + 1, k):
+        y = math.sqrt(d ** 2 - x ** 2)
+        answer += (y // k) + 1
+
+    return answer
+
+
+print(solution(2, 4))
+# 6 출력
+
+print(solution(1, 5))
+#  26 출력
